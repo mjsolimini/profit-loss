ba4bec7783762bd7f5111790c956fe1e0af87ee3
Michael Solimini <mjsolimini@gmail.com>
2017-06-12 20:58:04 -0400

    Initial Commit

---
+++ test-profit-loss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-profit-loss
+//
+//  Created by Michael Solimini on 6/12/17.
+//  Copyright © 2017 Alpha Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
